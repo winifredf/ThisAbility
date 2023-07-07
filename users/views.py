967e5320e62686e09db50e92000f1c2006e33524
Winifredf <Winifred.Dykes@hotmail.com>
2023-07-07 13:04:07 -0400

    defined register user usrnm pswd form submission

--- users/views.py
+++ users/views.py
@@ -3,4 +3,8 @@ from django.contrib.auth.models import User
 from .models import Employer
 
 # Create your views here.
+def register_user(request):
+    if request.method =='POST':
+        username = request.POST['username']
+        password = request.POST['password']
 
