e8ed84c35a2c9d548ca42eebef142aedd7236f9e
Winifredf <Winifred.Dykes@hotmail.com>
2023-07-07 01:35:28 -0400

    added str function

--- jobs/models.py
+++ jobs/models.py
@@ -9,3 +9,6 @@ class Job(models.Model):
     salary = models.DecimalField(max_digits=10, decimal_places=2)
     location = models.CharField(max_length=255)
     date = models.DateField()
+
+    def __str__(self):
+        return self.title
