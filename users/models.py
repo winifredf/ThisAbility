54fafc6d1d8f6fe320c58a190acaa05ca14afc91
Winifredf <Winifred.Dykes@hotmail.com>
2023-07-07 01:19:00 -0400

    added str method

--- users/models.py
+++ users/models.py
@@ -24,3 +24,6 @@ class Employer(models.Model):
     password = models.CharField(max_length=25)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     job = models.ForeignKey('Job', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
