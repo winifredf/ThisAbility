b62ccc29d2fa26bf12617f49e327399c01b0b965
Winifredf <Winifred.Dykes@hotmail.com>
2023-07-07 01:34:50 -0400

    created jobs model

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    requirements = models.TextField()
+    employer = models.ForeignKey(Employer, on_delete=models.CASCADE)
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+    location = models.CharField(max_length=255)
+    date = models.DateField()
