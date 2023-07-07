cd035c8f2570f5e9a2d4afa959d179265bc77fdc
Winifredf <Winifred.Dykes@hotmail.com>
2023-07-07 12:17:49 -0400

    updated CharField max lengths in User model

--- users/models.py
+++ users/models.py
@@ -2,15 +2,15 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
     age = models.PositiveIntegerField()
-    address = models.CharField(max_length=255)
+    address = models.CharField(max_length=210)
     gender = models.CharField(max_length=10)
     phone = models.CharField(max_length=20)
     email = models.EmailField()
-    username = models.CharField(max_length=50)
-    password = models.CharField(max_length=255)
-    abilities = models.CharField(max_length=255)
+    username = models.CharField(max_length=25)
+    password = models.CharField(max_length=25)
+    abilities = models.CharField(max_length=3000)
 
     def __str__(self):
         return self.name
