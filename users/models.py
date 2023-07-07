883825a9f97ba4178b9c56b339cbc9e59753f5da
Winifredf <Winifred.Dykes@hotmail.com>
2023-07-07 01:12:12 -0400

    added str method

--- users/models.py
+++ users/models.py
@@ -11,3 +11,7 @@ class User(models.Model):
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=255)
     abilities = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
