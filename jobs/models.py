0a8fba33ed89512ff9f3f73f1d316f59ac829bd5
Winifredf <Winifred.Dykes@hotmail.com>
2023-07-07 12:37:18 -0400

    updated Job.employer

--- jobs/models.py
+++ jobs/models.py
@@ -6,7 +6,7 @@ class Job(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     requirements = models.TextField()
-    employer = models.ForeignKey(Employer, on_delete=models.CASCADE)
+    employer = models.ForeignKey(Employer, on_delete=models.CASCADE, related_name='jobs')
     salary = models.DecimalField(max_digits=10, decimal_places=2)
     location = models.CharField(max_length=255)
     date = models.DateField()
