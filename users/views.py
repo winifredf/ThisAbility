e21606656807061dc62c1a5f9a80302e8b529f5d
Winifredf <Winifred.Dykes@hotmail.com>
2023-07-08 15:56:06 -0400

    deleted imports not used

--- users/views.py
+++ users/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from .models import Employer
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from .forms import EmployerRegistrationForm
