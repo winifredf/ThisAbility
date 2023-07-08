15b1ee893de7330fade4cf904c64493a835dd69c
Winifredf <Winifred.Dykes@hotmail.com>
2023-07-08 05:06:39 -0400

    created employer registration path

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,7 @@ app_name = 'users'
 urlpatterns = [
     path('register/', views.register_user, name='register'),
     path('login/', views.login_user, name='login'),
+    path('register/employer/', views.register_employer, name='employer_register'),
+
+
 ]
