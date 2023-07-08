ba758b1c0efdf844a12df6fc3e27fdf2f606d4ff
Winifredf <Winifred.Dykes@hotmail.com>
2023-07-08 05:27:57 -0400

    defined Employer Registration Form class with fields

--- users/forms.py
+++ users/forms.py
@@ -1,2 +1,10 @@
 from django import forms
 from .models import Employer
+
+class EmployerRegistrationForm(forms.ModelForm):
+    class Meta:
+        model = Employer
+        fields = ['name', 'address', 'phone', 'email', 'username', 'password', 'user']
+        widgets = {
+            'password': forms.PasswordInput()
+        }
