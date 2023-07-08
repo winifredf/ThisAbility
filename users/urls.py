edd0b17bfe57a9fc30eac7e91e8d6ec67f531b86
Winifredf <Winifred.Dykes@hotmail.com>
2023-07-08 12:23:18 -0400

    added logout url pattern

--- users/urls.py
+++ users/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('register/', views.register_user, name='register'),
     path('login/', views.login_user, name='login'),
     path('register/employer/', views.register_employer, name='employer_register'),
+    path('logout/', views.logout_user, name='logout'),
 
 
 ]
