3ee12b8e27a801f3f1c91db52121e279f94c8077
Winifredf <Winifred.Dykes@hotmail.com>
2023-07-09 01:06:07 -0400

    defined job list

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 from .models import Job
 
+def job_list(request):
+    jobs = Job.objects.all()
+    return render(request, 'jobs/job_list.html', {'jobs': jobs})
