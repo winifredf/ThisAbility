fe69cd3699af57dcdfea319f12fdb31c2b97cb12
Winifredf <Winifred.Dykes@hotmail.com>
2023-07-09 01:07:43 -0400

    defined job detail

--- jobs/urls.py
+++ jobs/urls.py
@@ -4,3 +4,7 @@ from .models import Job
 def job_list(request):
     jobs = Job.objects.all()
     return render(request, 'jobs/job_list.html', {'jobs': jobs})
+
+def job_detail(request, job_id):
+    job = Job.objects.get(pk=job_id)
+    return render(request, 'jobs/job_detail.html', {'job': job})
